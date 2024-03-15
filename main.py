8eab898bf1b24f7bd1ae1bca904852287bc6f3a0
FistQunami <161111378+FistQunami@users.noreply.github.com>
2024-03-15 05:48:28 +0000

    try to add something to github

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
 
 import uvicorn
@@ -15,6 +15,19 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+        <head>
+            <title>Button Example</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+            <button onclick="alert('Button Clicked!')">Click Me</button>
+        </body>
+    </html>
+    """
+
 if __name__ == "__main__":
    uvicorn.run(app, host="0.0.0.0", port=8000)
-  
